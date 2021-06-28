9c75e024a4859956adc5b9df0449aaab66c35792
mmcaulif <mmcaulif@tcd.ie>
2021-06-28 16:15:02 +0100

    started gui work

--- pmw_gui.py
+++ pmw_gui.py
@@ -1,18 +1,27 @@
 from tkinter import *
 from tkinter import ttk
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import csv
 
-array, plot_t, plot_other = [], [], []
+buffer, plot_t, plot_y, gyro, acc = [], [], [], [], []
 with open('pmw_log.csv', mode='r') as log:
     logreader = csv.reader(log, delimiter=',')
     for row in logreader:
         #print(row)
-        array.append(row)
+        buffer.append(row)
 
-for i in range(len(array)):
-    plot_t.append(array[i][1])
-    plot_other.append(array[i][2])
+for i in range(len(buffer)):
+    plot_t.append(buffer[i][1]) #get time
+    gyro.append(buffer[i][2:5]) #get gyro data
+    acc.append(buffer[i][5:8])  #get accelerometer data
 
-print(plot_t, "\n", plot_other)
+plot_random = np.random.normal(20, 5, len(plot_t))
+gyro = np.array(gyro)
+plot_t = np.array(plot_t)
+gyro = gyro.astype(float)
+
+plt.plot(plot_t, gyro)
+plt.xlabel('Time')
+plt.ylabel('Angle from origin')
+plt.show()
--- pmw_telemetry.py
+++ pmw_telemetry.py
@@ -12,40 +12,43 @@ board = MultiWii(serialPort)
 assert board != 1, "There's something wrong with the serial connection... check the logfile!"
 
 fig, (p1, p2) = plt.subplots(2)
+with open('pmw_log.csv', mode='w', newline='') as log:
+    logwriter = csv.writer(log)
+    for i in range(100):
+        try:
+            telemetry = board.getData(MultiWii.RAW_IMU)  #a = accelerometer, g = gyroscope
+            #attitude = board.getData(MultiWii.ATTITUDE)
+            gyro = [telemetry['ax'], telemetry['ay'], telemetry['az']]
+            acc = [telemetry['gx'], telemetry['gy'], telemetry['gz']]
+        except:
+            print("Packet error; defaulting to prev value")
 
-for i in range(25):
-    try:
-        telemetry = board.getData(MultiWii.RAW_IMU)  #a = accelerometer, g = gyroscope
-        #attitude = board.getData(MultiWii.ATTITUDE)
-        gyro = [telemetry['ax'], telemetry['ay'], telemetry['az']]
-        acc = [telemetry['gx'], telemetry['gy'], telemetry['gz']]
-    except:
-        print("Packet error; defaulting to prev value")
+        t = time.time() - inittime
+        print(i, t, "Gyro:",gyro, "Accelerometer:",acc)
+        logwriter.writerow([i, t, gyro[0], gyro[1], gyro[2], acc[0],acc[1],acc[2]])
+        time.sleep(0.25)
 
-
-    print("Gyro:",gyro,"Accelerometer:",acc)
-    t = time.time() - inittime
-    plot_x.append(t)
-    plot_y1.append(gyro)
-    plot_y2.append(acc)
-    p1.plot(plot_x, plot_y1)
-    p1.set(title='Gyroscope data')
-    p2.plot(plot_x, plot_y2)
-    p2.set(title='Accelerometer data')
-    plt.pause(0.05)
-    #time.sleep(0.05)
-
-    """print("Gyro:",gyro,"Accelerometer:",acc)
-    t = time.time() - inittime
-    plot_x.append(t)
-    plot_y2.append(acc)
-    plt.plot(plot_x, plot_y2)
-    plt.cla()
-    plt.pause(0.25)"""
+        """plot_x.append(t)
+        plot_y1.append(gyro)
+        plot_y2.append(acc)
+        p1.plot(plot_x, plot_y1)
+        p1.set(title='Gyroscope data')
+        p2.plot(plot_x, plot_y2)
+        p2.set(title='Accelerometer data')
+        plt.pause(0.05)
+        #time.sleep(0.05)
+    
+        print("Gyro:",gyro,"Accelerometer:",acc)
+        t = time.time() - inittime
+        plot_x.append(t)
+        plot_y2.append(acc)
+        plt.plot(plot_x, plot_y2)
+        plt.cla()
+        plt.pause(0.25)
 
 #plt.show()
 
-with open('pmw_log.csv', mode='w', newline='') as log:
+
     logwriter = csv.writer(log)
     for i in range(len(plot_x)):
-        logwriter.writerow([i, plot_x[i], plot_y1[i]])
+        logwriter.writerow([i, plot_x[i], plot_y1[i]])"""
